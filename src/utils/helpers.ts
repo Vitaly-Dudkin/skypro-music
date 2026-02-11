189a79f44a37250cd58df309d73fc80e68002280
Виталий <dudkin231@mail.ru>
2026-02-11 11:08:52 +0300

    added getUniqueValueByKey in helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,24 @@
 import { TrackType } from '@/sharedTypes/sharedTypes';
 
+export function getUniqueValueByKey(arr: TrackType[], key: keyof TrackType) {
+  const uniqueValues = new Set<string>();
+
+  arr.forEach((item) => {
+    const value = item[key];
+
+    if (Array.isArray(value)) {
+      value.forEach((v) => {
+        if (v) {
+          uniqueValues.add(v);
+        }
+      });
+    } else if (typeof value == 'string') {
+      uniqueValues.add(value);
+    }
+  });
+  return Array.from(uniqueValues);
+}
+
 export function formatTime(time: number) {
   const minutes = Math.floor(time / 60);
   const inputSeconds = Math.floor(time % 60);
