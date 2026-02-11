c7c9e8c2923050c8510ce36b1c18e22c50122144
Виталий <dudkin231@mail.ru>
2026-02-11 10:51:23 +0300

    added sharedTypes andd helpers

---
+++ src/utils/helpers.ts
@@ -0,0 +1,9 @@
+import { TrackType } from '@/sharedTypes/sharedTypes';
+
+export function formatTime(time: number) {
+  const minutes = Math.floor(time / 60);
+  const inputSeconds = Math.floor(time % 60);
+  const outputSeconds = inputSeconds < 10 ? `0${inputSeconds}` : inputSeconds;
+
+  return `${minutes}:${outputSeconds}`;
+}
